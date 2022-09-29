3b18fc0798471439ecead3960bf3a97a5c0d40bb
f-aris <113866175+f-aris@users.noreply.github.com>
2022-09-29 21:55:28 +0700

    1 file modified

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 import './style.css';
 
 // Write Javascript code!
+const user = prompt("Please enter your name!");
+
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>hello</h1>`;
+appDiv.innerHTML = `<h1>hello ${user}</h1>`;
